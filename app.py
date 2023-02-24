03bd45ab247833e5b2983bd5cf674b19c5246bac
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2023-02-24 20:49:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Server Works!'
+  
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+
+if __name__ == '__main__':
+    #server start port
+    app.run(port=5000)
